c4e251545453b11a2b7ca0a518a009b331d0cf31
Faheem <mfaheemamin4797@gmail.com>
2024-10-24 02:26:18 +0500

    Modifiers

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -5,14 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,18 +37,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    .width(250.dp)
-                    .height(300.dp)
-                    .fillMaxSize()
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically,
+
+                    .background(Color.Green)
+                    .fillMaxHeight(0.8f)
+                    .fillMaxWidth()
+                    .padding( start =  16.dp,top = 48.dp)
+                    .background(Color.Cyan)
+                    .border(5.dp,Color.Magenta,RoundedCornerShape(16.dp))
+                    .background(Color.Black)
+                    .padding( start =  5.dp,top = 5.dp)
+                    .border(5.dp,Color.Yellow,RoundedCornerShape(16.dp))
+                    .background(Color.White)
+                    .padding( start =  5.dp,top = 5.dp)
+                    .border(5.dp,Color.Blue,RoundedCornerShape(16.dp))
+                    .background(Color.Red)
+                    .padding( start =  5.dp,top = 5.dp)
+                ,
+//                    .requiredWidth(800.dp)
+//                horizontalArrangement = Arrangement.SpaceAround,
+//                verticalAlignment = Alignment.CenterVertically,
 //                horizontalAlignment = Alignment.CenterHorizontally,
 //                verticalArrangement = Arrangement.SpaceAround
             ) {
-                Text(text = "Hello")
+                Text(text = "Hello" ,
+//                    fontSize = 36.sp ,
+                    modifier = Modifier
+                        .clickable {
+
+                        }
+
+                )
+                Spacer(modifier = Modifier.height(20.dp))
                 Text(text = "World")
                 Text(text = "Pheeem")
             }
