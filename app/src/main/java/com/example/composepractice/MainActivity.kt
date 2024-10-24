c81617f048844035c02354920f3686b6737c964c
Faheem <mfaheemamin4797@gmail.com>
2024-10-24 06:40:57 +0500

    Styling Text

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -38,8 +38,16 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,14 +57,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_semibold, FontWeight.SemiBold),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
+
+            )
         setContent {
-            val painter = painterResource(id = R.drawable.zombatar)
-            Box(modifier = Modifier.padding(16.dp)) {
-                ImageCard(
-                    "pheeem",
-                    "content Description",
-                    painter
-                )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(
+                            color = Color.Green, fontSize = 50.sp
+                        )){
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(style = SpanStyle(
+                            color = Color.Green, fontSize = 50.sp
+                        )){
+                            append("C")
+                        }
+                        append("ompose")
+
+                    },
+                    Modifier.fillMaxWidth(),
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline,
+
+                    )
             }
 
 
