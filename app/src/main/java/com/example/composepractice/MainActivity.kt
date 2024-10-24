7fad6908b9119c85f41290914672ddc58f7d4586
Faheem <mfaheemamin4797@gmail.com>
2024-10-24 21:25:21 +0500

    state

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -3,160 +3,63 @@ package com.example.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.composepractice.ui.theme.ComposePracticeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        val fontFamily = FontFamily(
-            Font(R.font.lexend_thin, FontWeight.Thin),
-            Font(R.font.lexend_light, FontWeight.Light),
-            Font(R.font.lexend_regular, FontWeight.Normal),
-            Font(R.font.lexend_medium, FontWeight.Medium),
-            Font(R.font.lexend_semibold, FontWeight.SemiBold),
-            Font(R.font.lexend_bold, FontWeight.Bold),
-            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
 
-            )
+
+
         setContent {
-            Box(
-                modifier = Modifier
+            var color by remember{ mutableStateOf(Color.Yellow)}
+            Column (Modifier.fillMaxSize()) {
+                ColorBox(Modifier.weight(1f).fillMaxSize()){
+                    color = it
+                }
+                Box(Modifier.weight(1f)
                     .fillMaxSize()
-                    .background(Color(0xFF101010))
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(style = SpanStyle(
-                            color = Color.Green, fontSize = 50.sp
-                        )){
-                            append("J")
-                        }
-                        append("etpack ")
-                        withStyle(style = SpanStyle(
-                            color = Color.Green, fontSize = 50.sp
-                        )){
-                            append("C")
-                        }
-                        append("ompose")
-
-                    },
-                    Modifier.fillMaxWidth(),
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.Underline,
-
-                    )
+                    .background(color))
             }
-
 
         }
     }
 }
 
 @Composable
-fun ImageCard(
-    title: String,
-    contentDescription: String,
-    painter: Painter,
-    modifier: Modifier = Modifier
+fun ColorBox(modifier: Modifier = Modifier,
+             updateColor: (Color) -> Unit
+
 ) {
-    Card(
-        modifier = modifier.fillMaxWidth(0.5f),
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 10.dp
-        )
-    ) {
-        Box(
-            modifier.height(225.dp)
-        ) {
-            Image(
-                modifier = Modifier.fillMaxHeight(),
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 450f
-                        )
+    Box(
+        modifier
+            .background(Color.Red)
+            .clickable (indication = null,
+                interactionSource = remember { MutableInteractionSource() }) {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f,
                     )
-            )
-            Text(
-                text = title,
-                Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 10.dp),
-                color = Color.White,
-                fontSize = 16.sp,
-
-
                 )
 
-
-        }
-    }
-
+            })
 }
 
 
