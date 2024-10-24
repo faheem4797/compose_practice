058c3212644fd209ab7a48ea9c7a11d8141b84fb
Faheem <mfaheemamin4797@gmail.com>
2024-10-24 22:08:40 +0500

    SnackBars

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -6,60 +6,89 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-
-
-
         setContent {
-            var color by remember{ mutableStateOf(Color.Yellow)}
-            Column (Modifier.fillMaxSize()) {
-                ColorBox(Modifier.weight(1f).fillMaxSize()){
-                    color = it
+            val snackbarHostState = remember { SnackbarHostState() }
+            val scope = rememberCoroutineScope()
+            var textFieldState by remember { mutableStateOf("") }
+            val focusManager = LocalFocusManager.current
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                snackbarHost = { SnackbarHost(snackbarHostState) },
+            )
+            { paddingValue ->
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(paddingValue),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text("Enter your name")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+//                        modifier = Modifier.fillMaxHeight()
+
+
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        scope.launch {
+                            focusManager.clearFocus()
+                            val name = textFieldState
+                            textFieldState = ""
+                            snackbarHostState
+                                .showSnackbar("Hello $name")
+                        }
+                    }) {
+                        Text("Pls greet me")
+                    }
                 }
-                Box(Modifier.weight(1f)
-                    .fillMaxSize()
-                    .background(color))
             }
+
 
         }
     }
-}
-
-@Composable
-fun ColorBox(modifier: Modifier = Modifier,
-             updateColor: (Color) -> Unit
-
-) {
-    Box(
-        modifier
-            .background(Color.Red)
-            .clickable (indication = null,
-                interactionSource = remember { MutableInteractionSource() }) {
-                updateColor(
-                    Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        1f,
-                    )
-                )
-
-            })
 }
 
 
