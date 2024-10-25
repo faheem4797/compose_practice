86c6ce697623c01fb8ceedd94343464a292bdf24
Faheem <mfaheemamin4797@gmail.com>
2024-10-25 05:52:57 +0500

    Lists and Lazy Lists

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -5,15 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
@@ -32,7 +39,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -41,50 +51,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContent {
-            val snackbarHostState = remember { SnackbarHostState() }
-            val scope = rememberCoroutineScope()
-            var textFieldState by remember { mutableStateOf("") }
-            val focusManager = LocalFocusManager.current
+            val scrollState = rememberScrollState()
+           LazyColumn  {
+               itemsIndexed(
+                   listOf("This","is","Jetpack","Compose")
+               ){index,string ->
+                   Text(
+                       text = string + index.toString(),
+                       fontSize = 24.sp,
+                       fontWeight = FontWeight.Bold,
+                       textAlign =  TextAlign.Center,
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(vertical = 24.dp)
+                   )
 
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                snackbarHost = { SnackbarHost(snackbarHostState) },
-            )
-            { paddingValue ->
-                Column(
-                    Modifier
-                        .fillMaxSize()
-                        .padding(paddingValue),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                ) {
-                    TextField(
-                        value = textFieldState,
-                        label = {
-                            Text("Enter your name")
-                        },
-                        onValueChange = {
-                            textFieldState = it
-                        },
-                        singleLine = true,
-//                        modifier = Modifier.fillMaxHeight()
+               }
 
-
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            focusManager.clearFocus()
-                            val name = textFieldState
-                            textFieldState = ""
-                            snackbarHostState
-                                .showSnackbar("Hello $name")
-                        }
-                    }) {
-                        Text("Pls greet me")
-                    }
-                }
-            }
+           }
 
 
         }
