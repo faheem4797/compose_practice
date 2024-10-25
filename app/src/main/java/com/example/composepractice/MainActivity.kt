c5480b9aed4cac69ffae7cb3ddebd27277a487a5
Faheem <mfaheemamin4797@gmail.com>
2024-10-25 20:51:14 +0500

    Calculator App Design Done

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -3,104 +3,180 @@ package com.example.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarData
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
-import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
+
         setContent {
-            var sizeState by remember { mutableStateOf(200.dp) }
-            val size by animateDpAsState(
-                targetValue = sizeState,
-                animationSpec =
-                tween(durationMillis = 1000),
-                label = "dpAnimation"
-            )
-            val infiniteTransition = rememberInfiniteTransition()
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(durationMillis = 2000),
-                    repeatMode = RepeatMode.Reverse
-                )
-            )
-            Box(
+            var textFieldState by remember { mutableStateOf("5+8") }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Bottom),
+
                 modifier = Modifier
-                    .size(size)
-                    .background(color)
+                    .fillMaxSize()
+                    .background(Color.DarkGray)
+                    .padding(16.dp)
             ) {
-                Button(onClick = {
-                    sizeState += 50.dp
-                }, Modifier.align(Alignment.Center)) {
-                    Text("Increase Size")
-                }
+                Text(
+                    textFieldState,
+                    textAlign = TextAlign.End,
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp)
+                )
+
+                AddThreeButtonRow("AC","C","/",true)
+
+                AddDigitRow("7", "8", "9", "*")
+                AddDigitRow("4", "5", "6", "-")
+                AddDigitRow("1", "2", "3", "+")
+                AddThreeButtonRow("0",".","=",false)
             }
+
+
         }
     }
 }
 
+@Composable
+fun AddThreeButtonRow(
+    first: String,
+    second: String,
+    operator: String,
+    isTopRow: Boolean,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(72.dp)
+    ) {
+        Button(
+            onClick = {},
+            colors = ButtonDefaults
+                .buttonColors
+                    (containerColor = if (isTopRow)  Color.Gray else Color.Black  ),
+            modifier = Modifier
+                .weight(2f)
+                .fillMaxHeight()
+        ) { Text(first, fontSize = 24.sp, fontWeight = FontWeight.Bold) }
+        Button(
+            onClick = {},
+
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            colors = ButtonDefaults
+                .buttonColors
+                    (containerColor = if (isTopRow)  Color.Gray else Color.Black  ),
+
+            ) { Text(second, fontSize = 24.sp, fontWeight = FontWeight.Bold) }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFFA500)
+            )
+
+        ) { Text(operator, fontSize = 24.sp, fontWeight = FontWeight.Bold) }
+    }
+}
+
+@Composable
+fun AddDigitRow(
+    firstDigit: String,
+    secondDigit: String,
+    thirdDigit: String,
+    operator: String,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(72.dp)
+    ) {
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black
+            )
+        ) {
+
+            Text(firstDigit, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black
+            )
+        ) {
+
+            Text(secondDigit, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black
+            )
+        ) {
+
+            Text(thirdDigit, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFFA500)
+            )
+        ) {
+
+            Text(operator, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
 
 //@Preview(showBackground = true)
 //@Composable
