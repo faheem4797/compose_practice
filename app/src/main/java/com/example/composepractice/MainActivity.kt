27bec9a33ca9f198d9552a8809c770b39ec5718a
Faheem <mfaheemamin4797@gmail.com>
2024-10-30 05:34:46 +0500

    CheckBoxes Added

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -1,114 +1,39 @@
 package com.example.composepractice
 
-import android.annotation.SuppressLint
-
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.add
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.filled.ThumbUp
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.composepractice.ui.theme.ComposePracticeTheme
 import com.example.composepractice.ui.theme.GreenPracticeCompose
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -119,7 +44,8 @@ class MainActivity : ComponentActivity() {
 
             ComposePracticeTheme {
 //NavComponenet()
-                CardMaker()
+                LearnCheckBoxes()
+//                CardMaker()
 //                Box(
 ////                    modifier = Modifier.fillMaxSize(),
 ////                    contentAlignment = Alignment.BottomEnd
@@ -155,70 +81,104 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CardMaker() {
-    val videoList = listOf(
-        YouTubeDataClass(R.drawable.zombatar, "Zombatar", "pheeem"),
-        YouTubeDataClass(R.drawable.whatsapp, "WhatsApp", "pheeem"),
-        YouTubeDataClass(R.drawable.zombatar, "Zombatar", "pheeem"),
-        YouTubeDataClass(R.drawable.whatsapp, "WhatsApp", "pheeem"),
-        YouTubeDataClass(R.drawable.zombatar, "Zombatar", "pheeem"),
-        YouTubeDataClass(R.drawable.whatsapp, "WhatsApp", "pheeem")
-
-
-
+fun LearnCheckBoxes() {
+    val todoList: List<String> = listOf(
+        "Eat Breakfast", "Buy Groceries", "Go to Gym", "Eat Dinner"
     )
+    val context = LocalContext.current.applicationContext
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(32.dp)) {
+        todoList.forEach { it ->
+            var checkedItem by remember { mutableStateOf(false) }
+            Row {
+                Checkbox(
 
-    LazyColumn(Modifier.
-    fillMaxHeight().padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        items(videoList){
-            it -> YouTubeCard(it)
-        }
-    }
-}
 
-@Composable
-fun YouTubeCard(youTubeDataClass: YouTubeDataClass) {
-
-    Card(
-        modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 10.dp)
-            .fillMaxWidth(),
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-    ) {
-        Column(
-            modifier = Modifier.background(GreenPracticeCompose)
-        ) {
-            Image(
-                painter = painterResource(youTubeDataClass.thumbnail),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .aspectRatio(16f / 9f)
-            )
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(18.dp)
-            ) {
-                Text(
-                    youTubeDataClass.videoTitle,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp,
+                    checked = checkedItem, onCheckedChange = { checkedStatus ->
+                        checkedItem = checkedStatus
+                        Toast.makeText(
+                            context,
+                            "$it is $checkedStatus", Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 )
-                Spacer(Modifier.height(4.dp))
                 Text(
-                    youTubeDataClass.channelName,
-                    color = Color.White,
-                    maxLines = 1,
-//                    fontWeight = FontWeight.Bold,
-                    fontSize = 14.sp,
+                    it, fontSize = 22.sp,
+                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
             }
         }
+
     }
 }
+
+//@Composable
+//fun CardMaker() {
+//    val videoList = listOf(
+//        YouTubeDataClass(R.drawable.zombatar, "Zombatar", "pheeem"),
+//        YouTubeDataClass(R.drawable.whatsapp, "WhatsApp", "pheeem"),
+//        YouTubeDataClass(R.drawable.zombatar, "Zombatar", "pheeem"),
+//        YouTubeDataClass(R.drawable.whatsapp, "WhatsApp", "pheeem"),
+//        YouTubeDataClass(R.drawable.zombatar, "Zombatar", "pheeem"),
+//        YouTubeDataClass(R.drawable.whatsapp, "WhatsApp", "pheeem")
+//    )
+//
+//    LazyColumn(
+//        Modifier
+//            .fillMaxHeight()
+//            .padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)
+//    ) {
+//        items(videoList) { it ->
+//            YouTubeCard(it)
+//        }
+//    }
+//}
+//
+//@Composable
+//fun YouTubeCard(youTubeDataClass: YouTubeDataClass) {
+//
+//    Card(
+//        modifier = Modifier
+//            .padding(horizontal = 16.dp, vertical = 10.dp)
+//            .fillMaxWidth(),
+//        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+//    ) {
+//        Column(
+//            modifier = Modifier.background(GreenPracticeCompose)
+//        ) {
+//            Image(
+//                painter = painterResource(youTubeDataClass.thumbnail),
+//                contentDescription = null,
+//                contentScale = ContentScale.Crop,
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .aspectRatio(16f / 9f)
+//            )
+//            Column(
+//                Modifier
+//                    .fillMaxWidth()
+//                    .padding(18.dp)
+//            ) {
+//                Text(
+//                    youTubeDataClass.videoTitle,
+//                    color = Color.White,
+//                    fontWeight = FontWeight.Bold,
+//                    fontSize = 18.sp,
+//                )
+//                Spacer(Modifier.height(4.dp))
+//                Text(
+//                    youTubeDataClass.channelName,
+//                    color = Color.White,
+//                    maxLines = 1,
+////                    fontWeight = FontWeight.Bold,
+//                    fontSize = 14.sp,
+//                )
+//            }
+//        }
+//    }
+//}
 
 //@Composable
 //fun LearnFAB(){
